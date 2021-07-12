81aa70f8cf3114270987b2c63023dd628f3b7e5c
jiwon111 <dreamjiwon@naver.com>
2021-07-12 23:49:16 +0900

    middleware

--- ch06/learn-express/app.js
+++ ch06/learn-express/app.js
@@ -1,28 +1,51 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT||3000);
 
-app.use((req, res, next)=>{
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly: true,
+        secure: false,
+    },
+    name = 'session-cookie',
+}));
+
+app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됨');
     next();
 });
-
 app.get('/', (req, res, next) => {
-  //res.send('Hello Express');
-  //res.sendFile(path.join(__dirname, '/index.html'));//html파일 열리게 할 때
-  console.log('GET / 요청에서만 실행');
-  next();
-}, (req, res)=>{
-    throw new Error('에러 처리 미들웨어로 감')
+    console.log('GET / 요청에서만 실행됨');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 감');
 });
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
     console.error(err);
-    res.status(500).send(err.message);
+    res.status(500).send(err.messge);
 });
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
+/*app.get('/', (req, res) => {
+    //res.send('Hello, Express');
+    res.sendFile(path.join(__dirname, '/index.html'));
+});*/
+
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'), '번 포트에서 대기 중');
 });
